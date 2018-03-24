eaaafb8260e5d4e5ff2a13c983b1f37105762fcb
Roman Nikitenko <rnikiten@redhat.com>
2018-03-24 14:12:54 +0200

    Fix AppStateManagerTest
    Signed-off-by: Roman Nikitenko <rnikiten@redhat.com>

--- ide/che-core-ide-app/src/test/java/org/eclipse/che/ide/statepersistance/AppStateManagerTest.java
+++ ide/che-core-ide-app/src/test/java/org/eclipse/che/ide/statepersistance/AppStateManagerTest.java
@@ -11,10 +11,10 @@
 package org.eclipse.che.ide.statepersistance;
 
 import static org.fest.assertions.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.nullable;
 import static org.mockito.Mockito.atLeastOnce;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -32,9 +32,7 @@ import org.eclipse.che.api.promises.client.PromiseProvider;
 import org.eclipse.che.ide.api.parts.PerspectiveManager;
 import org.eclipse.che.ide.api.statepersistance.AppStateServiceClient;
 import org.eclipse.che.ide.api.statepersistance.StateComponent;
-import org.eclipse.che.ide.api.workspace.model.WorkspaceImpl;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
@@ -51,7 +49,8 @@ import org.mockito.Mock;
 @RunWith(GwtMockitoTestRunner.class)
 public class AppStateManagerTest {
 
-  private static final String WS_ID = "ws_id";
+  private static final String COMPONENT_ONE_ID = "component1";
+  private static final String COMPONENT_TWO_ID = "component2";
 
   @Mock private StateComponentRegistry stateComponentRegistry;
   @Mock private Provider<StateComponentRegistry> stateComponentRegistryProvider;
@@ -61,25 +60,23 @@ public class AppStateManagerTest {
   @Mock private Provider<StateComponent> component1Provider;
   @Mock private Provider<StateComponent> component2Provider;
   @Mock private Promise<Void> promise;
+  @Mock private Promise<String> getStatePromise;
   @Mock private JsonFactory jsonFactory;
   @Mock private AppStateServiceClient appStateService;
-  @Mock private JsonObject pref;
   @Mock private PromiseProvider promiseProvider;
 
   @Mock private Promise<Void> sequentialRestore;
 
   @Captor private ArgumentCaptor<Function<Void, Promise<Void>>> sequentialRestoreThenFunction;
 
-  @Captor private ArgumentCaptor<String> preferenceArgumentCaptor;
-
-  @Captor private ArgumentCaptor<String> jsonArgumentCaptor;
+  @Captor private ArgumentCaptor<String> saveStateArgumentCaptor;
 
   private AppStateManager appStateManager;
 
   @Before
   public void setUp() {
-    WorkspaceImpl workspace = mock(WorkspaceImpl.class);
-    when(workspace.getId()).thenReturn(WS_ID);
+    when(appStateService.getState()).thenReturn(getStatePromise);
+    when(getStatePromise.thenPromise((Function<String, Promise<Void>>) any())).thenReturn(promise);
 
     List<StateComponent> components = new ArrayList<>();
     components.add(component1);
@@ -92,9 +89,9 @@ public class AppStateManagerTest {
     when(component1Provider.get()).thenReturn(component1);
     when(component2Provider.get()).thenReturn(component2);
 
-    when(component1.getId()).thenReturn("component1");
-    when(component2.getId()).thenReturn("component2");
-    when(jsonFactory.parse(anyString())).thenReturn(pref = Json.createObject());
+    when(component1.getId()).thenReturn(COMPONENT_ONE_ID);
+    when(component2.getId()).thenReturn(COMPONENT_TWO_ID);
+    when(jsonFactory.parse(anyString())).thenReturn(Json.createObject());
     appStateManager =
         new AppStateManager(
             perspectiveManagerProvider,
@@ -105,13 +102,14 @@ public class AppStateManagerTest {
     appStateManager.readState();
   }
 
-  @Ignore
   @Test
-  public void shouldStoreStateInPreferences() throws Exception {
+  public void shouldStoreState() throws Exception {
     appStateManager.persistState();
+
+    verify(appStateService).saveState(saveStateArgumentCaptor.capture());
+    assertThat(saveStateArgumentCaptor.getValue()).isNotNull();
   }
 
-  @Ignore
   @Test
   public void shouldCallGetStateOnStateComponent() throws Exception {
     appStateManager.persistState();
@@ -119,58 +117,43 @@ public class AppStateManagerTest {
     verify(component2, atLeastOnce()).getState();
   }
 
-  @Ignore
-  @Test
-  public void shouldStoreStateByWsId() throws Exception {
-    appStateManager.persistState();
-    assertThat(preferenceArgumentCaptor.getValue()).isNotNull();
-    assertThat(preferenceArgumentCaptor.getValue()).isNotNull();
-    JsonObject object = Json.parse(jsonArgumentCaptor.getValue());
-    assertThat(object.hasKey(WS_ID)).isTrue();
-  }
-
-  @Ignore
   @Test
   public void shouldSaveStateInFile() throws Exception {
-    JsonObject object = Json.createObject();
-    object.put("key1", "value1");
-    when(component1.getState()).thenReturn(object);
+    JsonObject firstComponentState = Json.createObject();
+    firstComponentState.put("key1", "value1");
+    when(component1.getState()).thenReturn(firstComponentState);
+
+    JsonObject secondComponentState = Json.createObject();
+    secondComponentState.put("key2", "value2");
+    when(component2.getState()).thenReturn(secondComponentState);
 
     appStateManager.persistState();
 
     verify(component1).getState();
-    assertThat(jsonArgumentCaptor.getValue()).isNotNull().isNotEmpty();
+    verify(component2).getState();
+    verify(appStateService).saveState(saveStateArgumentCaptor.capture());
 
-    String value = jsonArgumentCaptor.getValue();
-    JsonObject jsonObject = Json.parse(value).getObject(WS_ID);
-    JsonObject workspace = jsonObject.getObject("workspace");
-    assertThat(workspace).isNotNull();
+    String json = saveStateArgumentCaptor.getValue();
+    assertThat(json).isNotNull();
+    JsonObject appState = Json.parse(json);
+    assertThat(appState).isNotNull();
 
-    JsonObject jsonObject1 = workspace.getObject("component1");
-    assertThat(jsonObject1.jsEquals(object)).isTrue();
+    JsonObject jsonObject1 = appState.getObject(COMPONENT_ONE_ID);
+    assertThat(jsonObject1.jsEquals(firstComponentState)).isTrue();
+
+    JsonObject jsonObject2 = appState.getObject(COMPONENT_TWO_ID);
+    assertThat(jsonObject2.jsEquals(secondComponentState)).isTrue();
   }
 
-  @Ignore
-  @Test
-  public void restoreShouldReadFromPreferences() throws Exception {
-    pref.put(WS_ID, Json.createObject());
-    appStateManager.restoreState();
-  }
-
-  @Ignore
   @Test
   public void restoreShouldCallLoadState() throws Exception {
-    JsonObject ws = Json.createObject();
-    pref.put(WS_ID, ws);
-    JsonObject workspace = Json.createObject();
-    ws.put("workspace", workspace);
-    JsonObject comp1 = Json.createObject();
-    workspace.put("component1", comp1);
-    comp1.put("key1", "value1");
-
+    JsonObject appState = Json.createObject();
+    JsonObject firstComponent = Json.createObject();
+    appState.put("component1", firstComponent);
+    firstComponent.put("key1", "value1");
     when(promiseProvider.resolve(nullable(Void.class))).thenReturn(sequentialRestore);
 
-    appStateManager.restoreState();
+    appStateManager.restoreState(appState);
 
     verify(sequentialRestore).thenPromise(sequentialRestoreThenFunction.capture());
     sequentialRestoreThenFunction.getValue().apply(null);
